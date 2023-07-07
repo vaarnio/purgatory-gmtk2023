b681b8f82c10031b97b421b192d09aff2cf8cd4a
Patrik Weck <patrik.weck@aalto.fi>
2023-07-08 00:28:04 +0300

    Add deletion of bullet

--- Bullet.gd
+++ Bullet.gd
@@ -9,3 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		get_parent().get_node(self.get_path()).queue_free()
