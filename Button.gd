1bd98baa1ede11b2519d0902ed0e9b1f25f5a07a
vaarnio <github@vaarn.io>
2023-07-07 22:16:47 +0300

    Added scene controller and level1 scene

---
+++ Button.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.pressed.connect(self._pressed)
+	pass # Replace with function body.
+
+func _pressed():
+	SceneController.goto_scene("res://level1.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
