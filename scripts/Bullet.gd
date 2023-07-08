7fd16a7822669ddfd5ae58b48313856683bc479a
Patrik Weck <patrik.weck@aalto.fi>
2023-07-08 15:23:31 +0300

    Add shooting mechanism

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -4,12 +4,23 @@ extends Area2D
 @export var move_dir : Vector2
 @export var start_pos : Vector2
 
+
+
+@export var type = "NormalBullet"
+
 # Called when the node enters the scene tree for the first time.
+func setup(a, b, c):
+	move_speed = a
+	move_dir = b
+	start_pos = c
+	
+	
+func changeType(a):
+	type = a
+
 func _ready():
-	global_position = Vector2(500, 100)
-	print(global_position)
-	print("ooo")
-	pass # Replace with function body.
+	global_position = start_pos
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,6 +29,6 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player")and type == "NormalBullet":
 		get_parent().get_node(self.get_path()).queue_free()
 		print("Test")
