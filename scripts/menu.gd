c57abf42520b6f1d520374ec4ef5f63904702924
Patrik Weck <patrik.weck@aalto.fi>
2023-07-08 21:33:21 +0300

    Add custom flare objects

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
-
+const obj_fire = preload("res://scenes/fire_particle.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_set_custom_cursor()
+	var new_fire = obj_fire.instantiate()
+	add_child.call_deferred(new_fire)
+	new_fire.global_position = Vector2(200, 800)
+	new_fire.get_node("CPUParticles2D").scale_amount_min = 12
+	new_fire.get_node("CPUParticles2D").scale_amount_max = 15
 	pass # Replace with function body.
 
 
