6299fd415b950d58c61665c32ec3cba7276e264c
Botond Ortutay <boortu@utu.fi>
2023-07-09 11:53:03 +0300

    bullet script cleaned up. Bullets now act as intended on collision

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -1,18 +1,22 @@
 extends Area2D
 
+
+### VARIABLES ###
 @export var move_speed : float = 300.0
 @export var move_dir : Vector2
 @export var start_pos : Vector2
 var velocity
 var type
-
 var texture_heal = preload("res://assets/bulletheal.png")
 var texture_attack = preload("res://assets/bulletdamage.png")
 var barrelArr = []
 var barrelsLocated = 0
 var min_distance = 10000
 
-# Called when the node enters the scene tree for the first time.
+
+### CUSTOM FUNCTIONS ###
+
+# Bullet initialiser. Direction is a coordinate (Vector2) toward which the bullet starts moving
 func setup(speed, direction, pos, set_type):
 	self.move_speed = speed
 	self.move_dir = direction
@@ -24,31 +28,33 @@ func setup(speed, direction, pos, set_type):
 	elif self.type == "AttackBullet":
 		$Sprite2D.texture = texture_attack
 
+func _killSelf():
+	get_parent().get_node(self.get_path()).queue_free()
+
+
+### GODOT DEFAULT FUNCTIONS ###
 func _ready():
 	global_position = start_pos
 	velocity = position.direction_to(move_dir) * move_speed
-	#print(velocity)
-	#pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position += velocity * delta
 
 
+### SIGNAL REACTION FUNCTIONS ###
 func _on_body_entered(body):
 	if body.is_in_group("Player")and self.type == "NormalBullet":
-		get_parent().get_node(self.get_path()).queue_free()
+		_killSelf()
 		PlayerVariables.heal_damage(5)
 		GameController.add_score(10)
-		print("Bullet hit player")
 	if body.is_in_group("barrel"):
+		_killSelf()
 		body._explode()
 		body._despawn()
 	if body.is_in_group("frenemy") and self.type == "AttackBullet":
+		_killSelf()
 		body.hit()
 	if body.is_in_group("blocking_enemy"):
+		_killSelf()
 		if self.type == "AttackBullet":
 			body.hit()
-		else:
-			get_parent().get_node(self.get_path()).queue_free()
